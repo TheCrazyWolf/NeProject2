b7d8fff1e7cc67d068f6513f284e098dd7047e11
kulagin <kulagin@107-SERV>
2022-05-11 12:46:42 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] array = new int[5];
+            array[0] = 2;
+            array[1] = 45;
+            array[2] = 5;
+
+            int[] chisla = { 0, 46, 67 } ;
+
+            Console.WriteLine(array[array.Length - 2]);
+
+            string[] peoples = { "Вася", "Катя", "Петя",
+            "Артем", "Валерия", "Гриша", "Марина"};
+
+            for (int i =0; i < peoples.Length; i++)
+            {
+                Console.WriteLine(peoples[i]);
+            }
+
+
+        }
+    }
+}
